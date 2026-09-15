db4c95d346c6fe5a79cd730bb9ab88a849c95191
Arnab Gupta <arnabgupta1111@gmail.com>
2026-09-15 11:28:11 +0600

    feat: edit , delete message and reaction to message

--- src/socket/handlers/message.handler.ts
+++ src/socket/handlers/message.handler.ts
@@ -3,6 +3,8 @@ import { logger } from "../../config/logger";
 import { Conversation } from "../../models/conversation.model";
 import { Message } from "../../models/message.model";
 import { AuthenticatedSocket } from "../middlewares/auth.middleware";
+import { socketError } from "../utils/errorHandler";
+import { ReactionEmoji } from "../../utils/constants";
 export const handleMessage = (
   io: SocketServer,
   socket: AuthenticatedSocket,
@@ -105,7 +107,6 @@ export const handleMessage = (
   );
 
   // 5. Message Read (Seen / Blue Tick)
-
   socket.on(
     "message_read",
     async (data: { messageId: string; conversationId: string }) => {
@@ -129,6 +130,130 @@ export const handleMessage = (
         );
       } catch (error) {
         logger.error("Error updating read status:", error);
+      }
+    },
+  );
+
+  //5. Edit Message
+  socket.on(
+    "edit_message",
+    async (
+      data: { messageId: string; conversationId: string; newText: string },
+      callback: Function,
+    ) => {
+      try {
+        const message = await Message.findById(data.messageId);
+        //1. Check if message is available
+        if (!message) {
+          throw new Error(data.messageId);
+        }
+        //2. Check only the message owner edit the message
+        if (message.sender.toString() !== userId?.toString()) {
+          throw new Error("Unauthorized to edit this message");
+        }
+
+        //3. Update Database
+
+        message.content = data.newText;
+        message.isEdited = true;
+        message.editedAt = new Date();
+        await message.save();
+
+        //4. Notify everyone in the room.
+        io.to(data.conversationId).emit("message_edited", {
+          messageId: message._id,
+          newText: data.newText,
+          editedAt: message.editedAt,
+        });
+
+        if (typeof callback === "function") callback({ status: "success" });
+        logger.info(`✏️ Message ${data.messageId} edited`);
+      } catch (error) {
+        socketError(socket, "edit_message", error);
+        if (typeof callback === "function") callback({ status: "error" });
+      }
+    },
+  );
+
+  //7. Delete Message (Soft Delete)
+  socket.on(
+    "delete_message",
+    async (data: { messageId: string; conversationId: string }, callback) => {
+      try {
+        const message = await Message.findById(data.messageId);
+        //1. Check if message is available
+        if (!message) {
+          throw new Error(data.messageId);
+        }
+        //2. Check only the message owner edit the message
+        if (message.sender.toString() !== userId?.toString()) {
+          throw new Error("Unauthorized to delete this message");
+        }
+        //Soft delete
+        message.content = "This message was deleted";
+        message.isDeleted = true;
+        message.deletedAt = new Date();
+        await message.save();
+
+        //Notify everyone in a room
+        io.to(data.conversationId).emit("message_deleted", {
+          messageId: message._id,
+          deletedAt: message.deletedAt,
+        });
+        if (typeof callback === "function") callback({ status: "success" });
+        logger.info(`🗑️ Message ${data.messageId} deleted`);
+      } catch (error) {
+        socketError(socket, "delete_message", error);
+        if (typeof callback === "function") callback({ status: "error" });
+      }
+    },
+  );
+  // 8. Toggle Reaction (Add / Remove)
+  socket.on(
+    "toggle_reaction",
+    async (
+      data: { messageId: string; conversationId: string; emoji: ReactionEmoji },
+      callback: Function,
+    ) => {
+      try {
+        const message = await Message.findById(data.messageId);
+        if (!message) throw new Error("Message not found");
+
+        // চেক করা ঐ ইউজার আগে থেকে কোনো রিঅ্যাকশন দিয়েছে কি না
+        const existingReactionIndex = message.reactions.findIndex(
+          (r) => r.user.toString() === userId?.toString(),
+        );
+
+        if (existingReactionIndex > -1) {
+          // যদি একই ইমোজি আবার দেয়, তবে রিমুভ করে দেওয়া (Toggle Off)
+          if (message.reactions[existingReactionIndex].emoji === data.emoji) {
+            message.reactions.splice(existingReactionIndex, 1);
+          } else {
+            // অন্য ইমোজি দিলে আগেরটা পরিবর্তন করে নতুনটা দেওয়া
+            message.reactions[existingReactionIndex].emoji = data.emoji;
+          }
+        } else {
+          // একদম নতুন রিঅ্যাকশন হলে অ্যাড করা
+          message.reactions.push({
+            user: userId as any,
+            emoji: data.emoji,
+            createdAt: new Date(),
+          });
+        }
+
+        await message.save();
+
+        // রুমের সবাইকে আপডেট পাঠানো
+        io.to(data.conversationId).emit("reaction_updated", {
+          messageId: message._id,
+          reactions: message.reactions,
+        });
+
+        if (typeof callback === "function") callback({ status: "success" });
+        logger.info(`👍 Reaction updated on message ${data.messageId}`);
+      } catch (error) {
+        socketError(socket, "toggle_reaction", error);
+        if (typeof callback === "function") callback({ status: "error" });
       }
     },
   );
